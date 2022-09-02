778d51ac37e64d89574efc24367549fdb70fb6d1
Reza Hadi Fairuztama <rreza510@gmail.com>
2022-09-02 16:11:43 +0700

    book data

---
+++ database/book.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"github.com/google/uuid"
+)
+
+type BookInterface interface {
+}
+
+func init() {
+	prepareStatements = append(prepareStatements)
+}
+
+type Book struct {
+	ID      uuid.UUID
+	Title   string
+	Author  string
+	Cover   URL
+	Summary string
+	Readers int
+	IsFav   bool
+}
